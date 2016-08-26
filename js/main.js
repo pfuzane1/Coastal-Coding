fe4e242a181daabc93c2dd01130f64c9a713acc6
Trent Ellingsen <trentellingsen@gmail.com>
2016-08-25 22:17:22 -0700

    updated the index to have most of the main content

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@
 $(window).on("load",function(){
    
     $(".loader-backdrop").fadeOut();               // Open Modal on Load or after delay
-        if($(".modal").length && ($(".modal").attr("data-open-onload"))=="true"){
+    /*if($(".modal").length && ($(".modal").attr("data-open-onload"))=="true"){
             setTimeout(function(){
                 $(".modal").modal();       
             }, $(".modal").attr("data-open-delay"));
-    }
+    }*/
 });
 
 jQuery(document).ready(function() {	
